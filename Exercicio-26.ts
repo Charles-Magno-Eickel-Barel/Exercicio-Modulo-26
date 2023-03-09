90ad023ed982bf7e36df9d39f6cbe379654567f2
seu-usersaneme <seu-email>
2023-03-09 20:44:27 -0300

    Primeiro commit

---
+++ Exercicio-26.ts
@@ -0,0 +1,14 @@
+function multiplicação(valor1: number, valor2: number): number {
+    let resultado: number
+    resultado = valor1 * valor2
+    return resultado
+}
+
+function saudacao(nome: string) {
+    let ola: string
+    ola = "Olá " + nome
+    return ola
+}
+
+console.log(multiplicação(2,2));
+console.log(saudacao("Charles"));
